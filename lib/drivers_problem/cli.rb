bac8036fb5c33f16dd8356121247a0b0a66a20e8
mbalsamo <github email address>
2018-07-18 14:44:43 -0400

    creat class methods and cli data input method

--- lib/drivers_problem/cli.rb
+++ lib/drivers_problem/cli.rb
@@ -1,5 +1,23 @@
 class DriversProblem::CLI
 
-  
+  def some_method
+    puts "Please feed me your data..."
+    data = gets.split("\n")
+
+    data.each do |info|
+      if info.split(" ").length > 2
+        resource = DriversProblem::Trip.new()
+      else
+        resource = DriversProblem::Driver.new()
+      end
+    end
+    # some kind of check driver only vs full string
+    if data.length > 1
+      resource = DriversProblem::Trip.new()
+    else
+      resource =
+    end
+    puts "#{resource.driver}"
+  end
 
 end
--- lib/drivers_problem/trips.rb
+++ lib/drivers_problem/trips.rb
@@ -1,28 +1,45 @@
 class DriversProblem::Trips
   attr_accessor :driver
-  attr_reader :start_time, :end_time, :miles_driven
+  attr_reader :start_time, :end_time, :miles_driven, :trip_time, :trip_speed
 
   def initialize(arg)
     data = arg.split(" ")
     @driver = Driver.new(data[0])
+    
   end
 
-  def trip_time()
+  def start_time(data)
+    starting = DateTime.parse(data[1])
+    @start_time = starting.hour*60 + starting.min
+  end
 
+  def end_time(data)
+    ending = DateTime.parse(data[2])
+    @end_time = ending.hour*60 + ending.min
+  end
+
+  def miles_driven(data)
+    @miles_driven = data[3]
+  end
+
+  def trip_time
+    @trip_time = @end_time - @start_time
+  end
+
+  def trip_speed
+    @trip_speed = ((@miles_driven/@trip_time)*60).round
   end
 end
 
-"Dan 07:15 07:45 17.3"
-(x miles/y min) * (60min/1hr)
-start_time = '07:15'.split(":")
-start_time[0]
-
-a = DateTime.parse("07:15")
-a_minutes = a.hour*60 + a.min
-
-b = DateTime.parse("07:45")
-b_minutes = b.hour*60 + b.min
-
-trip_time = b_minutes - a_minutes
-
-speed = (miles/trip_time)*(60)
+# "Dan 07:15 07:45 17.3"
+# (x miles/y min) * (60min/1hr)
+#
+# a = DateTime.parse("07:15")
+# a_minutes = a.hour*60 + a.min
+#
+# b = DateTime.parse("07:45")
+# b_minutes = b.hour*60 + b.min
+#
+# trip_time = b_minutes - a_minutes
+#
+# speed = (miles/trip_time)*(60)
