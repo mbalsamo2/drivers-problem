7a81b81cd0188ce86d9d5996b90c7bd796b5aa14
mbalsamo <github email address>
2018-07-18 13:45:45 -0400

    method testing data input for drivers speed

--- lib/drivers_problem/trips.rb
+++ lib/drivers_problem/trips.rb
@@ -7,8 +7,22 @@ class DriversProblem::Trips
     @driver = Driver.new(data[0])
   end
 
-  def start_time()
+  def trip_time()
+
   end
 end
 
 "Dan 07:15 07:45 17.3"
+(x miles/y min) * (60min/1hr)
+start_time = '07:15'.split(":")
+start_time[0]
+
+a = DateTime.parse("07:15")
+a_minutes = a.hour*60 + a.min
+
+b = DateTime.parse("07:45")
+b_minutes = b.hour*60 + b.min
+
+trip_time = b_minutes - a_minutes
+
+speed = (miles/trip_time)*(60)
