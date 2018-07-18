49b1f80e52c170b803ae11f08ce2f3b5d3e05fc2
mbalsamo <github email address>
2018-07-18 13:01:00 -0400

    lib directory setup

---
+++ lib/drivers_problem.rb
@@ -0,0 +1,6 @@
+require_relative "./drivers_problem/version"
+require_relative "./drivers_problem/cli"
+require_relative "./drivers_problem/drivers"
+require_relative "./drivers_problem/trips"
+
+require 'pry'
---
+++ lib/drivers_problem/version.rb
@@ -0,0 +1,3 @@
+module DriversProblem
+  VERSION = "0.1.0"
+end
